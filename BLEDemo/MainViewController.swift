9d24b5895843ee1028a3c1ffaaa37d3380751344
metawin99 <pai.pp61@gmail.com>
2019-08-25 17:21:22 +0700

    Commit
    Commit - Saved

--- BLEDemo/MainViewController.swift
+++ BLEDemo/MainViewController.swift
@@ -10,25 +10,14 @@ import UIKit
 import CoreBluetooth
 
 class MainViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
     var manager: CBCentralManager?
     var mainPeripheral: CBPeripheral?
     var mainCharacteristic: CBCharacteristic?
+    var characteristicsDic: CBUUID?
     
     
-    let BLEService = "FEA7D4D0-7C81-41A3-9B1E-553B6CC4A17C"
-    let BLECharacteristic = "FEA7D4D0-7C81-41A3-9B1E-553B6CC4A17C"
-    
-    private let UuidSerialService = "FEA7D4D0-7C81-41A3-9B1E-553B6CC4A17C"
-    private let UuidTx =            "FEA7D4D0-7C81-41A3-9B1E-553B6CC4A17C"
-    private let UuidRx =            "FEA7D4D0-7C81-41A3-9B1E-553B6CC4A17C"
-    var txCharacteristic: CBCharacteristic?
-    var rxCharacteristic: CBCharacteristic?
-    var serialPortPeripheral: CBPeripheral?
-
-    
-    
-    //let BLEService = "DFB0"
-    //let BLECharacteristic = "DFB1"
+    var isOpen = false
     
     @IBOutlet weak var recievedMessageText: UILabel!
     
@@ -84,38 +73,21 @@ class MainViewController: UIViewController, CBCentralManagerDelegate, CBPeripher
     }
     
     @objc func disconnectButtonPressed() {
-        //this will call didDisconnectPeripheral, but if any other apps are using the device it will not immediately disconnect
         manager?.cancelPeripheralConnection(mainPeripheral!)
     }
     
     @IBAction func sendButtonPressed(_ sender: AnyObject) {
-
-        let alert = UIAlertController(title: "Command", message: "Enter a iot command.", preferredStyle: .alert)
-        
-        alert.addTextField { (textField) in
-            textField.placeholder = "example : switch:true"
+        var str = "{switch:true}"
+        if isOpen {
+            str = "{switch:true}"
+            isOpen = !isOpen
+        } else {
+            str = "{switch:false}"
+            isOpen = !isOpen
         }
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
-            let dataToSend = textField?.text!.data(using: String.Encoding.utf8)
-            
-            self.serialPortPeripheral?.writeValue(dataToSend!, for: self.txCharacteristic!, type: CBCharacteristicWriteType.withResponse)
-            
-//            if (self.mainPeripheral != nil) {
-//                if let mainCharacteristicData = self.mainCharacteristic {
-//                    self.mainPeripheral?.writeValue(dataToSend!, for: mainCharacteristicData, type: CBCharacteristicWriteType.withoutResponse)
-//                    print("Success")
-//                } else {
-//                    print("failed")
-//                }
-//            } else {
-//                print("haven't discovered device yet")
-//            }
-        }))
-        
-        self.present(alert, animated: true, completion: nil)
-        
+        let data = Data(str.utf8)
+        self.mainPeripheral?.writeValue(data, for: mainCharacteristic!, type: .withResponse)
+
     }
     
     // MARK: - CBCentralManagerDelegate Methods
@@ -136,159 +108,23 @@ class MainViewController: UIViewController, CBCentralManagerDelegate, CBPeripher
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         
         for service in peripheral.services! {
-            
             print("Service found with UUID: " + service.uuid.uuidString)
-            
-            //device information service
-            if (service.uuid.uuidString == "180A") {
-                peripheral.discoverCharacteristics(nil, for: service)
-            }
-            
-            //GAP (Generic Access Profile) for Device Name
-            // This replaces the deprecated CBUUIDGenericAccessProfileString
-            if (service.uuid.uuidString == "1800") {
-                peripheral.discoverCharacteristics(nil, for: service)
-            }
-            
-            //Bluno Service
-            if (service.uuid.uuidString == BLEService) {
-                peripheral.discoverCharacteristics(nil, for: service)
-            }
-            
-            //Pai Test
-            
             peripheral.discoverCharacteristics(nil, for: service)
-            
         }
     }
-    
-//    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-//
-//        //get device name
-//        if (service.uuid.uuidString == "1800") {
-//
-//            for characteristic in service.characteristics! {
-//
-//                if (characteristic.uuid.uuidString == "2A00") {
-//                    peripheral.readValue(for: characteristic)
-//                    print("Found Device Name Characteristic")
-//                }
-//
-//            }
-//
-//        }
-//
-//        if (service.uuid.uuidString == "180A") {
-//
-//            for characteristic in service.characteristics! {
-//
-//                if (characteristic.uuid.uuidString == "2A29") {
-//                    peripheral.readValue(for: characteristic)
-//                    print("Found a Device Manufacturer Name Characteristic")
-//                } else if (characteristic.uuid.uuidString == "2A23") {
-//                    peripheral.readValue(for: characteristic)
-//                    print("Found System ID")
-//                }
-//
-//            }
-//
-//        }
-//
-//        if (service.uuid.uuidString == BLEService) {
-//
-//            for characteristic in service.characteristics! {
-//
-//                if (characteristic.uuid.uuidString == BLECharacteristic) {
-//                    //we'll save the reference, we need it to write data
-//                    mainCharacteristic = characteristic
-//
-//                    //Set Notify is useful to read incoming data async
-//                    peripheral.setNotifyValue(true, for: characteristic)
-//                    print("Found Bluno Data Characteristic")
-//                }
-//
-//            }
-//
-//        }
-//
-//        //Pai test
-//
-//        for characteristic in service.characteristics! {
-//            mainCharacteristic = characteristic
-//            //Set Notify is useful to read incoming data async
-//            peripheral.setNotifyValue(true, for: characteristic)
-//            print("Found Bluno Data Characteristic")
-//        }
-//
-//    }
-//
-//    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-//
-//
-//        if (characteristic.uuid.uuidString == "2A00") {
-//            //value for device name recieved
-//            let deviceName = characteristic.value
-//            print(deviceName ?? "No Device Name")
-//        } else if (characteristic.uuid.uuidString == "2A29") {
-//            //value for manufacturer name recieved
-//            let manufacturerName = characteristic.value
-//            print(manufacturerName ?? "No Manufacturer Name")
-//        } else if (characteristic.uuid.uuidString == "2A23") {
-//            //value for system ID recieved
-//            let systemID = characteristic.value
-//            print(systemID ?? "No System ID")
-//        } else if (characteristic.uuid.uuidString == BLECharacteristic) {
-//            //data recieved
-//            if(characteristic.value != nil) {
-//                let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
-//
-//                recievedMessageText.text = stringValue
-//            }
-//        }
-//
-//        // Pai test
-//
-//        let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
-//        recievedMessageText.text = stringValue
-//
-//
-//    }
-    
-    
-    
-    
-    //Pai
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        if let characteristics = service.characteristics {
-            for characteristic in characteristics {
-                // Tx:
-                if characteristic.uuid == CBUUID(string: UuidTx) {
-                    print("Tx char found: \(characteristic.uuid)")
-                    txCharacteristic = characteristic
-                }
-
-                // Rx:
-                if characteristic.uuid == CBUUID(string: UuidRx) {
-                    rxCharacteristic = characteristic
-                    if let rxCharacteristic = rxCharacteristic {
-                        print("Rx char found: \(characteristic.uuid)")
-                        serialPortPeripheral?.setNotifyValue(true, for: rxCharacteristic)
-                    }
-                }
-            }
+        for characteristic in service.characteristics! {
+            mainCharacteristic = characteristic
+            //Set Notify is useful to read incoming data async
+            peripheral.setNotifyValue(true, for: characteristic)
+            print("Found Bluno Data Characteristic")
         }
     }
-
+    
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        let rxData = characteristic.value
-        if let rxData = rxData {
-            let numberOfBytes = rxData.count
-            var rxByteArray = [UInt8](repeating: 0, count: numberOfBytes)
-            (rxData as NSData).getBytes(&rxByteArray, length: numberOfBytes)
-            print(rxByteArray)
-        }
+        let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
+        recievedMessageText.text = stringValue
     }
     
 }
-
