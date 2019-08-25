cac5220236fbc4b30a29aefb0cbad7d7b2ee2877
metawin99 <pai.pp61@gmail.com>
2019-08-25 17:34:38 +0700

    Commit
    Commit - Saved

--- BLEDemo/ScanTableViewController.swift
+++ BLEDemo/ScanTableViewController.swift
@@ -30,7 +30,6 @@ class ScanTableViewController: UITableViewController, CBCentralManagerDelegate {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return peripherals.count
     }
     
@@ -38,25 +37,23 @@ class ScanTableViewController: UITableViewController, CBCentralManagerDelegate {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "scanTableCell", for: indexPath)
         let peripheral = peripherals[indexPath.row]
-        cell.textLabel?.text = peripheral.name
-        
+        if peripheral.name == "" {
+            cell.textLabel?.text = "???"
+        } else {
+            cell.textLabel?.text = peripheral.name
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let peripheral = peripherals[indexPath.row]
-        
         manager?.connect(peripheral, options: nil)
     }
     
     // MARK: BLE Scanning
+    
     func scanBLEDevices() {
-        //manager?.scanForPeripherals(withServices: [CBUUID.init(string: parentView!.BLEService)], options: nil)
-        
-        //if you pass nil in the first parameter, then scanForPeriperals will look for any devices.
         manager?.scanForPeripherals(withServices: nil, options: nil)
-        
-        //stop scanning after 3 seconds
         DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
             self.stopScanForBLEDevices()
         }
@@ -67,42 +64,29 @@ class ScanTableViewController: UITableViewController, CBCentralManagerDelegate {
     }
     
     // MARK: - CBCentralManagerDelegate Methods
+    
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         if(!peripherals.contains(peripheral)) {
-            peripherals.append(peripheral)
+            if peripheral.name != "" && peripheral.name != nil {
+                peripherals.append(peripheral)
+            }
         }
-        
         self.tableView.reloadData()
     }
-    
-//    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : AnyObject], rssi RSSI: NSNumber) {
-//        
-//        if(!peripherals.contains(peripheral)) {
-//            peripherals.append(peripheral)
-//        }
-//        
-//        self.tableView.reloadData()
-//    }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print(central.state)
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        
-        //pass reference to connected peripheral to parent view
         parentView?.mainPeripheral = peripheral
         peripheral.delegate = parentView
         peripheral.discoverServices(nil)
-        
-        //set the manager's delegate view to parent so it can call relevant disconnect methods
         manager?.delegate = parentView
         parentView?.customiseNavigationBar()
-        
         if let navController = self.navigationController {
             navController.popViewController(animated: true)
         }
-        
         print("Connected to " +  peripheral.name!)
     }
     
